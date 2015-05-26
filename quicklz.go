67e193c2552751b7d2a8e3a86123c3e735578403
Damian Gryski <damian@gryski.com>
2015-05-26 10:14:36 +0200

    golint++
    Mostly snake -> camel, but a few source code transformations.
    
    Exported names still SHOUTING

--- quicklz.go
+++ quicklz.go
@@ -38,21 +38,20 @@ func headerLen(source []byte) int {
 
 func sizeDecompressed(source []byte) int {
 	if headerLen(source) == 9 {
-		return fast_read(source, 5, 4)
-	} else {
-		return fast_read(source, 2, 1)
+		return fastRead(source, 5, 4)
 	}
+	return fastRead(source, 2, 1)
+
 }
 
 func sizeCompressed(source []byte) int {
 	if headerLen(source) == 9 {
-		return fast_read(source, 1, 4)
-	} else {
-		return fast_read(source, 1, 1)
+		return fastRead(source, 1, 4)
 	}
+	return fastRead(source, 1, 1)
 }
 
-func fast_read(a []byte, i, numbytes int) int {
+func fastRead(a []byte, i, numbytes int) int {
 	l := 0
 	for j := 0; j < numbytes; j++ {
 		l |= int(a[i+j]) << (uint(j) * 8)
@@ -60,13 +59,13 @@ func fast_read(a []byte, i, numbytes int) int {
 	return l
 }
 
-func fast_write(a []byte, i, value, numbytes int) {
+func fastWrite(a []byte, i, value, numbytes int) {
 	for j := 0; j < numbytes; j++ {
 		a[i+j] = byte(value >> (uint(j) * 8))
 	}
 }
 
-func write_header(dst []byte, level int, compressible bool, size_compressed int, size_decompressed int) {
+func writeHeader(dst []byte, level int, compressible bool, sizeCompressed int, sizeDecompressed int) {
 	var cbit byte
 	if compressible {
 		cbit = 1
@@ -75,22 +74,22 @@ func write_header(dst []byte, level int, compressible bool, size_compressed int,
 	dst[0] |= byte(level << 2)
 	dst[0] |= (1 << 6)
 	dst[0] |= (0 << 4)
-	fast_write(dst, 1, size_decompressed, 4)
-	fast_write(dst, 5, size_compressed, 4)
+	fastWrite(dst, 1, sizeDecompressed, 4)
+	fastWrite(dst, 5, sizeCompressed, 4)
 }
 
 func compress(source []byte, level int) []byte {
-	var src int = 0
-	var dst int = DEFAULT_HEADERLEN + CWORD_LEN
-	var cword_val uint32 = 0x80000000
-	var cword_ptr = DEFAULT_HEADERLEN
-	var destination []byte = make([]byte, len(source)+400)
+	var src int
+	var dst = DEFAULT_HEADERLEN + CWORD_LEN
+	var cwordVal uint32 = 0x80000000
+	var cwordPtr = DEFAULT_HEADERLEN
+	var destination = make([]byte, len(source)+400)
 	var hashtable [][]int
-	var cachetable []int = make([]int, HASH_VALUES)
-	var hash_counter []byte = make([]byte, HASH_VALUES)
+	var cachetable = make([]int, HASH_VALUES)
+	var hashCounter = make([]byte, HASH_VALUES)
 	var d2 []byte
 	var fetch = 0
-	var last_matchstart = (len(source) - UNCONDITIONAL_MATCHLEN - UNCOMPRESSED_END - 1)
+	var lastMatchStart = (len(source) - UNCONDITIONAL_MATCHLEN - UNCOMPRESSED_END - 1)
 	var lits = 0
 
 	if level != 1 && level != 3 {
@@ -112,23 +111,23 @@ func compress(source []byte, level int) []byte {
 		return nil
 	}
 
-	if src <= last_matchstart {
-		fetch = fast_read(source, src, 3)
+	if src <= lastMatchStart {
+		fetch = fastRead(source, src, 3)
 	}
 
-	for src <= last_matchstart {
-		if (cword_val & 1) == 1 {
+	for src <= lastMatchStart {
+		if (cwordVal & 1) == 1 {
 			if src > 3*(len(source)>>2) && dst > src-(src>>5) {
 				d2 = make([]byte, len(source)+DEFAULT_HEADERLEN)
-				write_header(d2, level, false, len(source), len(source)+DEFAULT_HEADERLEN)
+				writeHeader(d2, level, false, len(source), len(source)+DEFAULT_HEADERLEN)
 				copy(d2[DEFAULT_HEADERLEN:], source)
 				return d2
 			}
 
-			fast_write(destination, cword_ptr, int(cword_val>>1)|0x80000000, 4)
-			cword_ptr = dst
+			fastWrite(destination, cwordPtr, int(cwordVal>>1)|0x80000000, 4)
+			cwordPtr = dst
 			dst += CWORD_LEN
-			cword_val = 0x80000000
+			cwordVal = 0x80000000
 		}
 
 		if level == 1 {
@@ -139,8 +138,8 @@ func compress(source []byte, level int) []byte {
 			cachetable[hash] = fetch
 			hashtable[hash][0] = src
 
-			if cache == 0 && hash_counter[hash] != 0 && (src-o > MINOFFSET || (src == o+1 && lits >= 3 && src > 3 && source[src] == source[src-3] && source[src] == source[src-2] && source[src] == source[src-1] && source[src] == source[src+1] && source[src] == source[src+2])) {
-				cword_val = ((cword_val >> 1) | 0x80000000)
+			if cache == 0 && hashCounter[hash] != 0 && (src-o > MINOFFSET || (src == o+1 && lits >= 3 && src > 3 && source[src] == source[src-3] && source[src] == source[src-2] && source[src] == source[src-1] && source[src] == source[src+1] && source[src] == source[src+2])) {
+				cwordVal = ((cwordVal >> 1) | 0x80000000)
 				if source[o+3] != source[src+3] {
 					f := 3 - 2 | (hash << 4)
 					destination[dst+0] = byte(f >> (0 * 8))
@@ -148,54 +147,54 @@ func compress(source []byte, level int) []byte {
 					src += 3
 					dst += 2
 				} else {
-					old_src := src
+					oldSrc := src
 					remaining := 255
 					if ln := (len(source) - UNCOMPRESSED_END - src + 1 - 1); ln <= 255 {
 						remaining = ln
 					}
 
 					src += 4
-					if source[o+src-old_src] == source[src] {
+					if source[o+src-oldSrc] == source[src] {
 						src++
-						if source[o+src-old_src] == source[src] {
+						if source[o+src-oldSrc] == source[src] {
 							src++
-							for source[o+(src-old_src)] == source[src] && (src-old_src) < remaining {
+							for source[o+(src-oldSrc)] == source[src] && (src-oldSrc) < remaining {
 								src++
 							}
 						}
 					}
 
-					matchlen := src - old_src
+					matchlen := src - oldSrc
 
 					hash <<= 4
 					if matchlen < 18 {
 						f := hash | (matchlen - 2)
-						// Inline fast_write
+						// Inline fastWrite
 						destination[dst+0] = byte(f >> (0 * 8))
 						destination[dst+1] = byte(f >> (1 * 8))
 						dst += 2
 					} else {
 						f := hash | (matchlen << 16)
-						fast_write(destination, dst, f, 3)
+						fastWrite(destination, dst, f, 3)
 						dst += 3
 					}
 				}
 				lits = 0
-				fetch = fast_read(source, src, 3)
+				fetch = fastRead(source, src, 3)
 			} else {
 				lits++
-				hash_counter[hash] = 1
+				hashCounter[hash] = 1
 				destination[dst] = source[src]
-				cword_val = (cword_val >> 1)
+				cwordVal = (cwordVal >> 1)
 				src++
 				dst++
 				fetch = (fetch>>8)&0xffff | int(source[src+2])<<16
 			}
 		} else {
-			fetch = fast_read(source, src, 3)
+			fetch = fastRead(source, src, 3)
 
 			var o, offset2 int
-			var matchlen, k, m, best_k int
+			var matchlen, k, m, bestK int
 			var c byte
 
 			remaining := 255
@@ -205,7 +204,7 @@ func compress(source []byte, level int) []byte {
 
 			hash := ((fetch >> 12) ^ fetch) & (HASH_VALUES - 1)
 
-			c = hash_counter[hash]
+			c = hashCounter[hash]
 			matchlen = 0
 			offset2 = 0
 			for k = 0; k < QLZ_POINTERS_3 && (int(c) > k || c < 0); k++ {
@@ -219,50 +218,50 @@ func compress(source []byte, level int) []byte {
 					if (m > matchlen) || (m == matchlen && o > offset2) {
 						offset2 = o
 						matchlen = m
-						best_k = k
+						bestK = k
 					}
 				}
 			}
 
-			_ = best_k
+			_ = bestK
 
 			o = offset2
 			hashtable[hash][c&(QLZ_POINTERS_3-1)] = src
 			c++
-			hash_counter[hash] = c
+			hashCounter[hash] = c
 
 			if matchlen >= 3 && src-o < 131071 {
 				offset := src - o
 				for u := 1; u < matchlen; u++ {
-					fetch = fast_read(source, src+u, 3)
+					fetch = fastRead(source, src+u, 3)
 					hash = ((fetch >> 12) ^ fetch) & (HASH_VALUES - 1)
-					c = hash_counter[hash]
-					hash_counter[hash]++
+					c = hashCounter[hash]
+					hashCounter[hash]++
 					hashtable[hash][c&(QLZ_POINTERS_3-1)] = src + u
 				}
 
 				src += matchlen
-				cword_val = ((cword_val >> 1) | 0x80000000)
+				cwordVal = ((cwordVal >> 1) | 0x80000000)
 
 				if matchlen == 3 && offset <= 63 {
-					fast_write(destination, dst, offset<<2, 1)
+					fastWrite(destination, dst, offset<<2, 1)
 					dst++
 				} else if matchlen == 3 && offset <= 16383 {
-					fast_write(destination, dst, (offset<<2)|1, 2)
+					fastWrite(destination, dst, (offset<<2)|1, 2)
 					dst += 2
 				} else if matchlen <= 18 && offset <= 1023 {
-					fast_write(destination, dst, ((matchlen-3)<<2)|(offset<<6)|2, 2)
+					fastWrite(destination, dst, ((matchlen-3)<<2)|(offset<<6)|2, 2)
 					dst += 2
 				} else if matchlen <= 33 {
-					fast_write(destination, dst, ((matchlen-2)<<2)|(offset<<7)|3, 3)
+					fastWrite(destination, dst, ((matchlen-2)<<2)|(offset<<7)|3, 3)
 					dst += 3
 				} else {
-					fast_write(destination, dst, ((matchlen-3)<<7)|(offset<<15)|3, 4)
+					fastWrite(destination, dst, ((matchlen-3)<<7)|(offset<<15)|3, 4)
 					dst += 4
 				}
 			} else {
 				destination[dst] = source[src]
-				cword_val = (cword_val >> 1)
+				cwordVal = (cwordVal >> 1)
 				src++
 				dst++
 			}
@@ -270,23 +269,23 @@ func compress(source []byte, level int) []byte {
 	}
 
 	for src <= len(source)-1 {
-		if (cword_val & 1) == 1 {
-			fast_write(destination, cword_ptr, int(cword_val>>1)|0x80000000, 4)
-			cword_ptr = dst
+		if (cwordVal & 1) == 1 {
+			fastWrite(destination, cwordPtr, int(cwordVal>>1)|0x80000000, 4)
+			cwordPtr = dst
 			dst += CWORD_LEN
-			cword_val = 0x80000000
+			cwordVal = 0x80000000
 		}
 
 		destination[dst] = source[src]
 		src++
 		dst++
-		cword_val = (cword_val >> 1)
+		cwordVal = (cwordVal >> 1)
 	}
-	for (cword_val & 1) != 1 {
-		cword_val = (cword_val >> 1)
+	for (cwordVal & 1) != 1 {
+		cwordVal = (cwordVal >> 1)
 	}
-	fast_write(destination, cword_ptr, int(cword_val>>1)|0x80000000, CWORD_LEN)
-	write_header(destination, level, true, len(source), dst)
+	fastWrite(destination, cwordPtr, int(cwordVal>>1)|0x80000000, CWORD_LEN)
+	writeHeader(destination, level, true, len(source), dst)
 
 	d2 = make([]byte, dst)
 	copy(d2, destination)
@@ -297,12 +296,12 @@ func decompress(source []byte) []byte {
 	size := sizeDecompressed(source)
 	src := headerLen(source)
 	var dst int
-	var cword_val = 1
+	var cwordVal = 1
 	destination := make([]byte, size)
 	hashtable := make([]int, 4096)
-	hash_counter := make([]byte, 4096)
-	last_matchstart := size - UNCONDITIONAL_MATCHLEN - UNCOMPRESSED_END - 1
-	last_hashed := -1
+	hashCounter := make([]byte, 4096)
+	lastMatchStart := size - UNCONDITIONAL_MATCHLEN - UNCOMPRESSED_END - 1
+	lastHashed := -1
 	var hash int
 	var fetch int
 
@@ -319,23 +318,23 @@ func decompress(source []byte) []byte {
 	}
 
 	for {
-		if cword_val == 1 {
-			cword_val = fast_read(source, src, 4)
+		if cwordVal == 1 {
+			cwordVal = fastRead(source, src, 4)
 			src += 4
-			if dst <= last_matchstart {
+			if dst <= lastMatchStart {
 				if level == 1 {
-					fetch = fast_read(source, src, 3)
+					fetch = fastRead(source, src, 3)
 				} else {
-					fetch = fast_read(source, src, 4)
+					fetch = fastRead(source, src, 4)
 				}
 			}
 		}
 
-		if (cword_val & 1) == 1 {
+		if (cwordVal & 1) == 1 {
 			var matchlen int
 			var offset2 int
 
-			cword_val = cword_val >> 1
+			cwordVal = cwordVal >> 1
 
 			if level == 1 {
 				hash = (fetch >> 4) & 0xfff
@@ -379,39 +378,39 @@ func decompress(source []byte) []byte {
 			destination[dst+1] = destination[offset2+1]
 			destination[dst+2] = destination[offset2+2]
 
-			for i := 3; i < matchlen; i += 1 {
+			for i := 3; i < matchlen; i++ {
 				destination[dst+i] = destination[offset2+i]
 			}
 			dst += matchlen
 
 			if level == 1 {
-				fetch = fast_read(destination, last_hashed+1, 3) // destination[last_hashed + 1] | (destination[last_hashed + 2] << 8) | (destination[last_hashed + 3] << 16);
-				for last_hashed < dst-matchlen {
-					last_hashed++
+				fetch = fastRead(destination, lastHashed+1, 3) // destination[lastHashed + 1] | (destination[lastHashed + 2] << 8) | (destination[lastHashed + 3] << 16);
+				for lastHashed < dst-matchlen {
+					lastHashed++
 					hash = ((fetch >> 12) ^ fetch) & (HASH_VALUES - 1)
-					hashtable[hash] = last_hashed
-					hash_counter[hash] = 1
-					fetch = (fetch >> 8 & 0xffff) | (int(destination[last_hashed+3]) << 16)
+					hashtable[hash] = lastHashed
+					hashCounter[hash] = 1
+					fetch = (fetch >> 8 & 0xffff) | (int(destination[lastHashed+3]) << 16)
 				}
-				fetch = fast_read(source, src, 3)
+				fetch = fastRead(source, src, 3)
 			} else {
-				fetch = fast_read(source, src, 4)
+				fetch = fastRead(source, src, 4)
 			}
-			last_hashed = dst - 1
+			lastHashed = dst - 1
 		} else {
-			if dst <= last_matchstart {
+			if dst <= lastMatchStart {
 				destination[dst] = source[src]
-				dst += 1
-				src += 1
-				cword_val = cword_val >> 1
+				dst++
+				src++
+				cwordVal = cwordVal >> 1
 
 				if level == 1 {
-					for last_hashed < dst-3 {
-						last_hashed++
-						fetch2 := fast_read(destination, last_hashed, 3)
+					for lastHashed < dst-3 {
+						lastHashed++
+						fetch2 := fastRead(destination, lastHashed, 3)
 						hash = ((fetch2 >> 12) ^ fetch2) & (HASH_VALUES - 1)
-						hashtable[hash] = last_hashed
-						hash_counter[hash] = 1
+						hashtable[hash] = lastHashed
+						hashCounter[hash] = 1
 					}
 					fetch = fetch>>8&0xffff | int(source[src+2])<<16
 				} else {
@@ -419,15 +418,15 @@ func decompress(source []byte) []byte {
 				}
 			} else {
 				for dst <= size-1 {
-					if cword_val == 1 {
+					if cwordVal == 1 {
 						src += CWORD_LEN
-						cword_val = 0x80000000
+						cwordVal = 0x80000000
 					}
 
 					destination[dst] = source[src]
 					dst++
 					src++
-					cword_val = cword_val >> 1
+					cwordVal = cwordVal >> 1
 				}
 				return destination
 			}
